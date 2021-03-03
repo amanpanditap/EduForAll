b359b4ae27bd16b0265e121ff2e1fc95af78fc97
Vaishnavi Mahajan <mvaishnavi411@gmail.com>
2021-03-04 00:20:13 +0530

    add answer n add question page

--- ensenanza/forum/urls.py
+++ ensenanza/forum/urls.py
@@ -7,7 +7,7 @@ app_name = 'forum'
 urlpatterns = [
     path('',views.forumLanding.as_view(),name='forumLanding'),
     path('ask',views.addQuestion.as_view(),name='addQuestion'),
-    path('answer/<int:QID>',views.addAnswer.as_view(),name='addAnswer'),
-    path('question/<int:QID>',views.queAns.as_view(),name='queAns'),
+    path('answer/<int:id>',views.addAnswer.as_view(),name='addAnswer'),
+    path('question/<int:id>',views.queAns.as_view(),name='queAns'),
     path('myquestions',views.myQuestions.as_view(),name='myQuestions'),
 ]
--- ensenanza/forum/views.py
+++ ensenanza/forum/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from forum.models import answer, question, student
+from django.urls import reverse_lazy
 # Create your views here.
 
 #contains all questions sorted by latest first
@@ -21,17 +23,13 @@ class forumLanding(View):
         Questions = Questions.order_by('-created')
         message = {}
         message['questions']= Questions
-        message['medium'] = medium
-        message['subject'] = subject
-        message['board'] = board
-        message['acadYear'] = acadYear
         return render(request,template_name,message)
 
 #display particular question and all answers to it
 class queAns(View):
-    def get(self, request, QID, template_name='queAns.html'):
+    def get(self, request, id, template_name='queAns.html'):
         message = {}
-        Question = question.objects.filter(id=QID)
+        Question = question.objects.filter(id=id)
         Question = Question[0]
         message['question']=Question
         answers = answer.objects.filter(whichQuestion=Question)
@@ -43,43 +41,63 @@ class queAns(View):
 class addQuestion(View):
 
     def get(self, request, template_name='addQuestion.html'):
+        if request.user.is_anonymous:
+            return redirect('Login')
         return render(request,template_name)
 
     def post(self, request, template_name='addQuestion.html'):
-        questionBy = student.objects.filter(user=request.user)
-        questionBy = questionBy[0]
-        title = request.POST.get('title')
-        body = request.POST.get('body')
-        medium = request.POST.get('medium')
-        acadYear = request.POST.get('acadYear')
-        subject = request.POST.get('subject')
-        Question = question(questionBy=questionBy,title=title,body=body,medium=medium,acadYear=acadYear,subject=subject)
-        Question.save()
-        message = {}
-        message['error_message']='Question submitted successfully'
-        #message['questions']=question.objects.all().order_by('-created')
-        return render(request, template_name,message)
+        if request.user.is_anonymous:
+            return redirect('Login')
+        try:
+            questionBy = student.objects.filter(user=request.user)
+            questionBy = questionBy[0]
+            title = request.POST.get('title')
+            body = request.POST.get('body')
+            medium = request.POST.get('medium')
+            acadYear = request.POST.get('acadYear')
+            subject = request.POST.get('subject')
+            Question = question(questionBy=questionBy,title=title,body=body,medium=medium,acadYear=acadYear,subject=subject)
+            Question.save()
+            #return redirect(queAns)
+        except:
+            message = {}
+            message['error_message']='Something went wrong, try again.'
+            #message['questions']=question.objects.all().order_by('-created')
+            return render(request,template_name,message)
+        return redirect('forum:forumLanding')
 
 #answer a question
 class addAnswer(View):
 
-    def get(self, request, QID, template_name='addAnswer.html'):
+    def get(self, request, id, template_name='addAnswer.html'):
+        if request.user.is_anonymous:
+            return redirect('Login')
         message = {}
-        message['question']=question.objects.filter(id=QID)[0]
+        message['question']=question.objects.filter(id=id)[0]
         return render(request,template_name,message)
     
-    def post(self, request, QID, template_name='addAnswer.html'):
-        body = request.POST.get('body')
-        answerBy = student.objects.filter(user=request.user)
-        answerBy = answerBy[0]
-        whichQuestion = question.objects.filter(id=QID)
-        whichQuestion = whichQuestion[0]
-        Answer = answer(whichQuestion=whichQuestion,answerBy=answerBy,body=body)
-        Answer.save()
+    def post(self, request, id, template_name='addAnswer.html'):
+        if request.user.is_anonymous:
+            return redirect('Login')
+        try:
+            body = request.POST.get('body')
+            answerBy = student.objects.filter(user=request.user)
+            print(answerBy)
+            answerBy = answerBy[0]
+            whichQuestion = question.objects.filter(id=id)
+            whichQuestion = whichQuestion[0]
+            Answer = answer(whichQuestion=whichQuestion,answerBy=answerBy,body=body)
+            Answer.save()
+        except:
+            message = {}
+            message['error_message'] = 'Something went wrong, try again.'
+            #message['question']= whichQuestion
+            #Answers = answer.objects.filter(whichQuestion=whichQuestion)
+            #message['answers']= Answers.order_by('-created')
+            return render(request,template_name,message)
         message = {}
-        message['error_message'] = 'Answered successfully.'
-        message['question']= whichQuestion
-        message['answers']= answer.objects.filter(whichQuestion=whichQuestion)
+        message['error_message'] = 'Answer submitted successfully.'
+        #return redirect(reverse_lazy('forum:queAns whichQuestion.id'))
         return render(request,template_name,message)
 
 #view questions asked by particular user
