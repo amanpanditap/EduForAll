054e4aa7fe9731c035acaca8f15d62dcab0e5866
Devang Kamble <risingentropy20@gmail.com>
2021-03-06 16:13:40 +0530

    resources

---
+++ ensenanza/static/resources.js
@@ -0,0 +1,440 @@
+const allBooks = [{
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Mathematics Part 1",
+        "Location": "/files/books/english/C10EEganit1.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Mathematics Part 2",
+        "Location": "/files/books/english/C10EEganit2.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "English",
+        "Location": "/files/books/english/C10EEkumarbharti.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Marathi",
+        "Location": "/files/books/english/C10EEaksharMarathi.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Hindi",
+        "Location": "/files/books/english/C10EElokHindi.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "History and Political Science",
+        "Location": "/files/books/english/C10EEitihasRajya.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Geography",
+        "Location": "/files/books/english/C10EEbhugol.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Science and Technology Part 1",
+        "Location": "/files/books/english/C10EEscience1.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Science and Technology Part 2",
+        "Location": "/files/books/english/C10EEscience2.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "BagAurdu",
+        "Location": "/files/books/english/C10EEbagAurdu.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Tarufe Urdu",
+        "Location": "/files/books/english/C10EEtarufeUrdu.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Gujarathi Garavi Sampurn",
+        "Location": "/files/books/english/C10EEgaraviSampurn.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Gujarathi Sahitya Sanyut",
+        "Location": "/files/books/english/C10EEsahityaSanyut.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Defence Studies",
+        "Location": "/files/books/english/C10EEsanrakshanKarypustk.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Sanskrit Sampurn",
+        "Location": "/files/books/english/C10EEsanskritSampurn.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Sanskrit Sankrit",
+        "Location": "/files/books/english/C10EEsanskritSankrit.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Telugu",
+        "Location": "/files/books/english/C10EEsaralTelugu.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Sindhu Devnagri",
+        "Location": "/files/books/english/C10EEsindhuDevnagri.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Sindhu Sindhi Arebic",
+        "Location": "/files/books/english/C10EEsindhuSindhiArebic.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Kannada",
+        "Location": "/files/books/english/C10EEsugamKannada.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "English",
+        "Title": "Self Development and Art Appreciation",
+        "Location": "/files/books/english/C10EEswavikasKalaraswad.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Mathematics Part 1",
+        "Location": "/files/books/hindi/C10HHganit1.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Mathematics Part 2",
+        "Location": "/files/books/hindi/C10HHganit2.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Hindi",
+        "Location": "/files/books/hindi/C10HHkumarbharti.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Marathi",
+        "Location": "/files/books/hindi/C10HHaksharMarathi.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "English",
+        "Location": "/files/books/hindi/C10HHenglish.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "History and Political Science",
+        "Location": "/files/books/hindi/C10HHitihasRajya.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Geography",
+        "Location": "/files/books/hindi/C10HHbhugol.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Science and Technology Part 1",
+        "Location": "/files/books/hindi/C10HHscience1.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Science and Technology Part 2",
+        "Location": "/files/books/hindi/C10HHscience2.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Gujarathi Garavi Sampurn",
+        "Location": "/files/books/hindi/C10HHgarawiSampurn.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Gujarathi Sahitya Sanyut",
+        "Location": "/files/books/hindi/C10HHsahityaSanyuktGuj.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Defence Studies",
+        "Location": "/files/books/hindi/C10HHsanrakshan.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Sanskrit Sampurn",
+        "Location": "/files/books/hindi/C10HHsanskritSampurn.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Sanskrit Sanyukt",
+        "Location": "/files/books/hindi/C10HHsanskritSanyukt.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Telugu",
+        "Location": "/files/books/hindi/C10HHsaralTelugu.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Sindhu Devnagri",
+        "Location": "/files/books/hindi/C10HHsindhuDevnagri.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Sindhu Arebic",
+        "Location": "/files/books/hindi/C10HHsindhuArebic.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Hindi",
+        "Title": "Self Development and Art Appreciation",
+        "Location": "/files/books/hindi/C10HHswavikasKalaraswad.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Mathematics Part 1",
+        "Location": "/files/books/marathi/C10MMganit1.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Mathematics Part 2",
+        "Location": "/files/books/marathi/C10MMganit2.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Marathi",
+        "Location": "/files/books/marathi/C10MMkumarbharti.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Hindi",
+        "Location": "/files/books/marathi/C10MMlokHindi.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "English",
+        "Location": "/files/books/marathi/C10MMenglish.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "History and Political Science",
+        "Location": "/files/books/marathi/C10MMitihasRajya.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Geography",
+        "Location": "/files/books/marathi/C10MMbhugol.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Science and Technology Part 1",
+        "Location": "/files/books/marathi/C10MMscience1.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Science and Technology Part 2",
+        "Location": "/files/books/marathi/C10MMscience2.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Gujarathi Garavi Sampurn",
+        "Location": "/files/books/marathi/C10MMgaraviSampurnGujrati.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Gujarathi Sahitya Sanyut",
+        "Location": "/files/books/marathi/C10MMsahityaSanyuktGujarati.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Defence Studies",
+        "Location": "/files/books/marathi/C10MMsanrakshanPustak.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Sanskrit Sampurn",
+        "Location": "/files/books/marathi/C10MMsanskritSampun.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Sanskrit Sanyukt",
+        "Location": "/files/books/marathi/C10MMsanskritSanyukt.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Telugu",
+        "Location": "/files/books/marathi/C10MMsaralTelugu.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Sindhu Devnagri",
+        "Location": "/files/books/marathi/C10MMsindhuDevnag.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Sindhu Arebic",
+        "Location": "/files/books/marathi/C10MMsindhuArebic.pdf"
+    },
+    {
+        "Board": "SSC",
+        "Class": 10,
+        "Medium": "Marathi",
+        "Title": "Self Development and Art Appreciation",
+        "Location": "/files/books/marathi/C10MMswvikasKalaras.pdf"
+    }
+]
+
+
+
+getBooks = () => {
+    let board = document.querySelector('#board').value;
+    let medium = document.querySelector('#medium').value;
+    let acadYear = document.querySelector('#acadYear').value;
+    //let subject = document.querySelector('#subject').value;
+    let booksToShow = []
+
+    for (let i = 0; i < allBooks.length; i++) {
+        //console.log(allBooks[i].Board, board)
+        //console.log(allBooks[i].Class, Number(acadYear))
+        //console.log(allBooks[i].Medium, medium)
+        if (allBooks[i].Board === board && allBooks[i].Class === Number(acadYear) && allBooks[i].Medium === medium) {
+            booksToShow.push(allBooks[i]);
+        }
+    }
+
+    let content = '';
+
+    booksToShow.forEach(p => {
+        content += `
+        <div class="col-sm-3">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" style="text-align: center">${p.Title}</h5>
+            <p class="card-text" style="text-align: center">Board: ${p.Board}<br>Class: ${p.Class}<br>Medium: ${p.Medium}</p>
+            <div class="text-center">
+            <a href=${p.Location} class="btn btn-primary" style="text-align: center">Download</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  `
+    });
+    
+    document.querySelector("#wantedBooks").innerHTML = content;
+
+}
